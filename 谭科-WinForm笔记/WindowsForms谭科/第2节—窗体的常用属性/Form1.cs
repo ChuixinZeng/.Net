e3826941e32a663b4384474e91094324afac1e18
曾垂鑫 <zengchuixin@126.com>
2017-06-01 22:59:09 +0800

    更新第2节代码段，关于size窗体大小

--- 谭科-WinForm笔记/WindowsForms谭科/第2节—窗体的常用属性/Form1.cs
+++ 谭科-WinForm笔记/WindowsForms谭科/第2节—窗体的常用属性/Form1.cs
@@ -10,18 +10,32 @@ using System.Windows.Forms;
 
 namespace 第2节_窗体的常用属性
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
         }
 
-        //单击窗体时，设置窗体的宽度/高度
+        
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.Width = 800;//宽度
-            this.Height = 500;//高度
+            
+        }
+
+        //单击窗体时，设置窗体的宽度/高度
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            //this.Width = 800;//宽度
+            //this.Height = 500;//高度
+
+            //也可以通过size属性进行设置
+            //如果报错的话，把鼠标放到错误的字段上，按F1，可以直接打开官方的帮助
+            //下面的size，鼠标放在上面，显示是一个重载，按F12可以看到相关信息
+
+            this.Size = new Size(800, 600);
+
+
         }
     }
 }
--- 谭科-WinForm笔记/WindowsForms谭科/第2节—窗体的常用属性/Program.cs
+++ 谭科-WinForm笔记/WindowsForms谭科/第2节—窗体的常用属性/Program.cs
@@ -16,7 +16,7 @@ namespace 第2节_窗体的常用属性
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
