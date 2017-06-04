51e1eb55374cad08e8a9570878da52a5b9ad6748
曾垂鑫 <zengchuixin@126.com>
2017-06-04 22:08:03 +0800

    更新第6节Picturebox控件

---
+++ 谭科-WinForm笔记/WindowsForms谭科/第6节—PictureBox图像控件/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace 第6节_PictureBox图像控件
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnRand = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox
+            // 
+            this.picBox.Location = new System.Drawing.Point(13, 13);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(759, 536);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBox.TabIndex = 0;
+            this.picBox.TabStop = false;
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(793, 13);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(75, 23);
+            this.btnFirst.TabIndex = 1;
+            this.btnFirst.Text = "第一张";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(793, 53);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 2;
+            this.btnPrev.Text = "前一张";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(793, 93);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "下一张";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(793, 133);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(75, 23);
+            this.btnLast.TabIndex = 4;
+            this.btnLast.Text = "最后一张";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnRand
+            // 
+            this.btnRand.Location = new System.Drawing.Point(793, 463);
+            this.btnRand.Name = "btnRand";
+            this.btnRand.Size = new System.Drawing.Size(75, 23);
+            this.btnRand.TabIndex = 5;
+            this.btnRand.Text = "启动随机";
+            this.btnRand.UseVisualStyleBackColor = true;
+            this.btnRand.Click += new System.EventHandler(this.btnRand_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 561);
+            this.Controls.Add(this.btnRand);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.picBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnRand;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 谭科-WinForm笔记/WindowsForms谭科/第6节—PictureBox图像控件/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace 第6节_PictureBox图像控件
+{
+    public partial class Form1 : Form
+    {
+        string[] picNames = null;//用于保存图片路径
+        int index = 0;//表示当前显示的图片的索引号
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //窗体加载时，应该讲所有图片的路径获得并保存到一个数组中
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            picNames = Directory.GetFiles(@"E:\GitRepository\VSCodeForC-\谭科-WinForm笔记\WindowsForms谭科\Pics");
+            this.index = 0;
+            ShowPic(); //调用下面设置的方法，显示第一张图片
+
+        }
+
+        //显示图片的方法
+
+        private void ShowPic()
+        {
+
+            //picBox.Load(picNames[this.index]);//用来显示index索引号对应的图片
+            picBox.Image = Image.FromFile(picNames[this.index]);//第二种显示图片的方法
+
+        }
+
+        //第一张图片
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            this.index = 0;
+            ShowPic();
+        }
+
+        //前一张
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            if (this.index == 0)//如果已经是第一张，index为0,这是如果不使用IF的话，就会减到负一，下标越界，导致失败异常
+            {
+                this.index = picNames.Length - 1;//就跳到最后一张去
+
+            }
+            else {
+                this.index--;
+            }
+            
+            ShowPic();
+
+        }
+
+        //最后一张
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            this.index = picNames.Length - 1;
+            ShowPic();
+        }
+
+        //下一张
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (this.index == picNames.Length-1)//如果已经是最后一张
+            {
+                this.index = 0;//就跳到第一张去
+
+            }
+            else
+            {
+                this.index++;
+            }
+            ShowPic();
+        }
+
+
+       //tick事件跟timer控件的interval属性搭配使用，interval的单位是毫秒，默认是100毫秒,1000毫秒等于1秒
+       //每隔interval毫秒，执行一次tick事件里面的代码
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();//产生一个随机对象
+           this.index = r.Next(0, picNames.Length);//取0到图片数量
+            ShowPic();
+
+        }
+
+        private void btnRand_Click(object sender, EventArgs e)
+        {
+            if (btnRand.Text == "启动随机")//点击启动随机按钮，开始随机显示图片，同时按钮变成关闭随机
+            {
+                timer1.Enabled = true;
+                btnRand.Text = "关闭随机";
+            }
+            else
+            {
+                timer1.Enabled = false;
+                btnRand.Text = "启动随机";
+            }
+        }
+    }
+}
