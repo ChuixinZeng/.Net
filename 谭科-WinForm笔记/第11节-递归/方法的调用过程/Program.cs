41c788d336a900cc98950184782573b047df68f8
曾垂鑫 <zengchuixin@126.com>
2017-06-09 22:35:38 +0800

    递归

---
+++ 谭科-WinForm笔记/第11节-递归/方法的调用过程/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 方法的调用过程
+{
+    class Program
+    {
+        static int i = 0;
+        static void Main(string[] args)
+        {
+            M1();
+            Console.ReadKey();
+        }
+        static void M1()
+        {
+            i++;
+            Console.WriteLine("M1方法");
+            if (i <= 5)
+            {
+                M1();//函数直接或间接地调用了自己，这就叫递归。
+            }
+
+            //使用递归，必须有一个结束的条件，也叫递归出口。
+        }
+        static void M2()
+        {
+            Console.WriteLine("M2方法");
+            M3();
+        }
+
+        static void M3()
+        {
+            Console.WriteLine("M3方法");
+        }
+    }
+}
---
+++ 谭科-WinForm笔记/第11节-递归/第11节-递归/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 第11节_递归
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //阶乘：
+            //5的阶乘：5*4*3*2*1
+            //4的阶乘：4*3*2*1
+            //3的阶乘：3*2*1
+            Console.WriteLine(GetFact(4));
+        }
+        static int GetFact(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            return n * GetFact(n - 1);
+        }
+    }
+}
