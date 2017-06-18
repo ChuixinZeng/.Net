21683e30bb3e6055a4ce9bdd046a182ec7929be5
曾垂鑫 <zengchuixin@126.com>
2017-06-18 11:49:37 +0800

    更新第14节注释信息

--- 谭科-WinForm笔记/第14节-MessageBox消息框/第14节-MessageBox消息框/Form1.cs
+++ 谭科-WinForm笔记/第14节-MessageBox消息框/第14节-MessageBox消息框/Form1.cs
@@ -22,12 +22,13 @@ namespace 第14节_MessageBox消息框
         //关于默认按钮：   MessageBoxDefaultButton
         private void btnOK_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(null, "你喜欢我吗？", "提问", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+            MessageBox.Show(null, "你喜欢我吗？", "提问", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);//Buttion2代表将messagebox的第二个按钮设置成默认按钮（默认按钮变成否）
         }
         //退出：返回值的演示
         private void btnQuit_Click(object sender, EventArgs e)
         {
             DialogResult dr = MessageBox.Show(null, "确定退出吗？", "退出", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+            //DialogResult是MessageBox里面支持的参数
             if (dr == DialogResult.Yes)//用户点了“是”
             {
                 this.Close();
