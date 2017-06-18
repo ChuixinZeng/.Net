ad9acce6146736e98957e58f89757c9f9989b7c3
曾垂鑫 <zengchuixin@126.com>
2017-06-18 12:33:55 +0800

    更新第16节课件

--- 谭科-WinForm笔记/第15节-OpenFileDialog对话框/第15节-OpenFileDialog对话框/Form1.cs
+++ 谭科-WinForm笔记/第15节-OpenFileDialog对话框/第15节-OpenFileDialog对话框/Form1.cs
@@ -24,7 +24,7 @@ namespace 第15节_OpenFileDialog对话框
             //设置标题
             opdOpenFile.Title = "谭科要打开文件了";
 
-            //设置文件筛选器
+            //设置文件筛选器，|前面是定义下拉列表的，|后面才是筛选的文件后缀
             //opdOpenFile.Filter = "所有文件(*.*)|*.*|文本文件(*.txt)|*.txt|Excel文件(*.xls;*.xlsx)|*.xls;*.xls*|媒体文件(*.mp4)|*.mp4";
             opdOpenFile.Filter = "文本文件(*.txt)|*.txt";
 
@@ -33,7 +33,7 @@ namespace 第15节_OpenFileDialog对话框
             if (dr == DialogResult.OK)//用户选择了文件，并点击“打开”按钮
             {
                 //获取用户选择的文件路径
-                txtFileName.Text = opdOpenFile.FileName;
+                txtFileName.Text = opdOpenFile.FileName; //单个文件用FileName，多个文件用FileNames
             }
 
             //读取文件内容并显示到文本框
@@ -59,9 +59,9 @@ namespace 第15节_OpenFileDialog对话框
         private void btnPath_Click(object sender, EventArgs e)
         {
             //MessageBox.Show("测试.xlsx");//相对路径：有时候会被其他对话框更改
-                                         //这种使用相对路径的做法是不准确的。
+            //这种使用相对路径的做法是不准确的。
 
-            //建议的做法：获取程序（exe）所在路径
+            //建议的做法：获取程序（exe）所在路径，依赖前面导入的.Reflection命名空间
             MessageBox.Show(Assembly.GetExecutingAssembly().Location);//当前exe程序的全路径
         }
     }
