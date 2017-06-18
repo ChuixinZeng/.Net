b15302259c839ce9a79d9a424c90eafab3830ce0
曾垂鑫 <zengchuixin@126.com>
2017-06-18 15:06:28 +0800

    更新第17节和第18节

--- 谭科-WinForm笔记/第18节-记事本-新建和另存/MyNotePad/MyNotePad/Form1.cs
+++ 谭科-WinForm笔记/第18节-记事本-新建和另存/MyNotePad/MyNotePad/Form1.cs
@@ -12,7 +12,7 @@ namespace MyNotePad
 {
     public partial class frmMain : Form
     {
-        private string fileName = "";       //保存文件名，因为文件名后面很多菜单都要调用，所以定义到这里
+        private string fileName = "";       //保存文件名
         private bool isSaved = false;       //标记是否保存
 
         public frmMain()
@@ -33,11 +33,11 @@ namespace MyNotePad
             if (ofdOpenFile.ShowDialog() == DialogResult.OK)
             {
                 fileName = ofdOpenFile.FileName;        //保存文件名
-                //创建读取文本文件的流，StreamReader类来自于IO类，只能操作文本文件，文件比较大的时候，适合用这个
+                //创建读取文本文件的流
                 using (StreamReader sr = new StreamReader(fileName, Encoding.Default))
                 {
                     rtbContents.Text = sr.ReadToEnd();  //读取文件数据并显示
-                    this.Text = string.Format("{0} - 记事本", Path.GetFileName(fileName)); //标题栏显示打开的文件的文件名
+                    this.Text = string.Format("{0} - 记事本", Path.GetFileName(fileName));
                     this.isSaved = true;        //标记文件已经保存
                 }
             }
@@ -45,6 +45,12 @@ namespace MyNotePad
 
         //保存
         private void tsmSaveFile_Click(object sender, EventArgs e)
+        {
+            WriteTextToFile();
+        }
+
+        //保存数据到文件
+        private void WriteTextToFile()
         {
             if (fileName == "")//未保存过的新文件
             {
@@ -54,20 +60,49 @@ namespace MyNotePad
                     fileName = sfdSave.FileName;            //获取用户指定的文件名
                     this.Text = string.Format("{0} - 记事本", Path.GetFileName(fileName));
                 }
+                //创建写入文本文件的流
+                using (StreamWriter sw = new StreamWriter(fileName, false, Encoding.Default))
+                {
+                    sw.WriteLine(rtbContents.Text);     //把内容写入流对象
+                    sw.Flush();             //把流中的数据刷新到文件
+                    this.isSaved = true;    //标记文件已经保存
+                }
             }
-            WriteTextToFile(); //直接调用下面写好的方法
         }
 
-        //保存数据到文件，这是封装好的方法，可以很好滴避免代码冗余问题
-        private void WriteTextToFile()
+        //新建
+        private void tsmNewFile_Click(object sender, EventArgs e)
         {
-            //创建写入文本文件的流
-            using (StreamWriter sw = new StreamWriter(fileName, false, Encoding.Default))
+            if (this.isSaved == false)//文件未保存
             {
-                sw.WriteLine(rtbContents.Text);     //把内容写入流对象
-                sw.Flush();             //把流中的数据刷新到文件
-                this.isSaved = true;    //标记文件已经保存
+                DialogResult dr = MessageBox.Show(null, "文件尚未保存，是否保存？", "警告", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button3);
+                if (dr == DialogResult.Cancel)//取消
+                {
+                    return;
+                }
+                if (dr==DialogResult.Yes)//是
+                {
+                    WriteTextToFile();
+                }
             }
+            //初始化窗体
+            this.Text = "无标题 - 记事本";
+            rtbContents.Clear();
+            this.isSaved = false;
+            this.fileName = "";
+        }
+
+        //文本发生变化的事件
+        private void rtbContents_TextChanged(object sender, EventArgs e)
+        {
+            this.isSaved = false;
+        }
+
+        //另存为
+        private void tssmSaveNew_Click(object sender, EventArgs e)
+        {
+            fileName = "";
+            WriteTextToFile();
         }
     }
 }
